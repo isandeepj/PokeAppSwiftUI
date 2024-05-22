13524090cd27a2290f9d377ef5954c7db7cf94e6
Sandeep <24805252+isandeepj@users.noreply.github.com>
2024-05-22 09:37:34 +0530

    Initial Commit

---
+++ PokeAppSwiftUI/PokeAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokeAppSwiftUIApp.swift
+//  PokeAppSwiftUI
+//
+//  Created by Sandeep on 22/05/24.
+//
+
+import SwiftUI
+
+@main
+struct PokeAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
